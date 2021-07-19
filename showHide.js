782d0c554576527f2fe25dd8f44a607831a8df43
sstevens <stesh969@student.otago.ac.nz>
2021-07-19 14:34:17 +1200

    Finished Javascript show/hide

--- showHide.js
+++ showHide.js
@@ -1,11 +1,30 @@
 function showHideDetails() {
-    alert("Not implemented yet");
+    var paragraphs, p, image, i;
+    paragraphs = this.parentNode.getElementsByTagName("p");
+    image = this.parentNode.getElementsByTagName("img");
+    for (p = 0; p < paragraphs.length; p+=1) {
+        // Show or hide paragraphs[p]
+        if (paragraphs[p].style.display === "none") {
+            paragraphs[p].style.display = "block";
+        } else {
+            paragraphs[p].style.display = "none";
+        }
+    }
+    for (i = 0; i < image.length; i+=1) {
+        // Show or hide paragraphs[p]
+        if (image[i].style.display === "none") {
+            image[i].style.display = "block";
+        } else {
+            image[i].style.display = "none";
+        }
+    }
 }
 function setup() {
     var films, f, title;
     films = document.getElementsByClassName("film");
     for (f = 0; f < films.length; f += 1) {
         title = films[f].getElementsByTagName("h3")[0];
+        title.style.cursor = "pointer"; //Let's user know they can click on title
         title.onclick = showHideDetails;
     }
 }
