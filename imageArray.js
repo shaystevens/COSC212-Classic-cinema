6d15a45926bf52064baf33c5122a09dfbfdc532a
sstevens <stesh969@student.otago.ac.nz>
2021-07-22 10:00:06 +1200

    Now cycles through an array of objects

--- imageArray.js
+++ imageArray.js
@@ -1,25 +1,41 @@
-"use strict";
 var imageList, imageIndex;
 
 function nextImage() {
+    "use strict";
     if(imageIndex === imageList.length){
         imageIndex = 0;
     }
-
-    document.getElementById("images").innerHTML.src = imageList[imageIndex];
+    document.getElementById("images").innerHTML = imageList[imageIndex];
     imageIndex++;
 // Need some code in here using imageList and imageIndex
 }
 
 function setup() {
+    "use strict";
+    var classic, scifi, hitchcock;
     imageList = [];
-    imageList.push("images/Metropolis.jpg");
-    imageList.push("images/Plan_9_from_Outer_Space.jpg");
-    imageList.push("images/Vertigo.jpg");
+    classic = new MovieCategory("Classic Films", "images/Metropolis.jpg", "classic.html");
+    scifi = new MovieCategory("Science Fiction and Horror", "images/Plan_9_from_Outer_Space.jpg", "scifi.html");
+    hitchcock = new MovieCategory("Alfred Hitchcock", "images/The_Birds.jpg", "hitchcock.html");
+
+    imageList.push(classic.makeHTML());
+    imageList.push(scifi.makeHTML());
+    imageList.push(hitchcock.makeHTML());
     imageIndex = 0;
+    window.onload = nextImage;
     setInterval(nextImage, 2000);
+}
+
+function MovieCategory(title, image, page) {
+    this.title = title;
+    this.image = image;
+    this.page = page;
+    this.makeHTML = function() {
+        return "<a href='" + page + "'><figure><img src='" + image + "' ><figcaption>'" + title + "'</figcaption></figure></a>";
+    };
 }
 
 if (document.getElementById) {
     window.onload = setup;
+
 }
