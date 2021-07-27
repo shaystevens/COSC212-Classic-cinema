92eab10f9812aecd5c91744961a7e960ee47542b
sstevens <stesh969@student.otago.ac.nz>
2021-07-28 09:56:56 +1200

    Lab 5, module and event listeners completed

--- imageArray.js
+++ imageArray.js
@@ -1,39 +1,51 @@
-var imageList, imageIndex;
-function nextImage() {
+var imageArray = (function(){
     "use strict";
-    if(imageIndex === imageList.length){
-        imageIndex = 0;
+    var pub = {}; //An object array filled with public functions
+
+    var imageList, imageIndex;
+    function nextImage() {
+        if(imageIndex === imageList.length){
+            imageIndex = 0;
+        }
+        document.getElementById("images").innerHTML = imageList[imageIndex];
+        imageIndex++;
     }
-    document.getElementById("images").innerHTML = imageList[imageIndex];
-    imageIndex++;
-// Need some code in here using imageList and imageIndex
-}
 
-function setup() {
-    "use strict";
-    var classic, scifi, hitchcock;
-    imageList = [];
-    classic = new MovieCategory("Classic Films", "images/Metropolis.jpg", "classic.html");
-    scifi = new MovieCategory("Science Fiction and Horror", "images/Plan_9_from_Outer_Space.jpg", "scifi.html");
-    hitchcock = new MovieCategory("Alfred Hitchcock", "images/The_Birds.jpg", "hitchcock.html");
+    pub.setup = function() {
+        var classic, scifi, hitchcock;
+        imageList = [];
+        classic = new MovieCategory("Classic Films", "images/Metropolis.jpg", "classic.html");
+        scifi = new MovieCategory("Science Fiction and Horror", "images/Plan_9_from_Outer_Space.jpg", "scifi.html");
+        hitchcock = new MovieCategory("Alfred Hitchcock", "images/The_Birds.jpg", "hitchcock.html");
 
-    imageList.push(classic.makeHTML());
-    imageList.push(scifi.makeHTML());
-    imageList.push(hitchcock.makeHTML());
-    imageIndex = 0;
-    nextImage();
-    setInterval(nextImage, 2000);
-}
-
-function MovieCategory(title, image, page) {
-    this.title = title;
-    this.image = image;
-    this.page = page;
-    this.makeHTML = function() {
-        return "<a href='" + this.page + "'><figure><img src='" + this.image + "'><figcaption>" + this.title + "</figcaption></figure></a>";
+        imageList.push(classic.makeHTML());
+        imageList.push(scifi.makeHTML());
+        imageList.push(hitchcock.makeHTML());
+        imageIndex = 0;
+        nextImage();
+        setInterval(nextImage, 2000);
     };
-}
+
+    function MovieCategory(title, image, page) {
+        this.title = title;
+        this.image = image;
+        this.page = page;
+        this.makeHTML = function() {
+            return "<a href='" + this.page + "'><figure><img src='" + this.image + "'><figcaption>" + this.title + "</figcaption></figure></a>";
+        };
+    }
+
+    return pub;
+
+}());
+
 
 if (document.getElementById) {
-    window.onload = setup;
+    if (window.addEventListener) {
+        window.addEventListener('load', imageArray.setup);
+    } else if (window.attachEvent) {
+        window.attachEvent('onload', imageArray.setup);
+    } else {
+        alert("Could not attach 'imageArray.setup' to the 'window.onload' event");
+    }
 }
--- showHide.js
+++ showHide.js
@@ -1,33 +1,48 @@
-function showHideDetails() {
-    var paragraphs, p, image, i;
-    paragraphs = this.parentNode.getElementsByTagName("p");
-    image = this.parentNode.getElementsByTagName("img");
-    for (p = 0; p < paragraphs.length; p+=1) {
-        // Show or hide paragraphs[p]
-        if (paragraphs[p].style.display === "none") {
-            paragraphs[p].style.display = "block";
-        } else {
-            paragraphs[p].style.display = "none";
+var showHide = (function(){
+    "use strict";
+    var pub = {}; //An object array filled with public functions
+
+    function showHideDetails() {
+        var paragraphs, p, image, i;
+        paragraphs = this.parentNode.getElementsByTagName("p");
+        image = this.parentNode.getElementsByTagName("img");
+        for (p = 0; p < paragraphs.length; p+=1) {
+            // Show or hide paragraphs[p]
+            if (paragraphs[p].style.display === "none") {
+                paragraphs[p].style.display = "block";
+            } else {
+                paragraphs[p].style.display = "none";
+            }
+        }
+        for (i = 0; i < image.length; i+=1) {
+            // Show or hide paragraphs[p]
+            if (image[i].style.display === "none") {
+                image[i].style.display = "block";
+            } else {
+                image[i].style.display = "none";
+            }
         }
     }
-    for (i = 0; i < image.length; i+=1) {
-        // Show or hide paragraphs[p]
-        if (image[i].style.display === "none") {
-            image[i].style.display = "block";
-        } else {
-            image[i].style.display = "none";
+    pub.setup = function() {
+        var films, f, title;
+        films = document.getElementsByClassName("film");
+        for (f = 0; f < films.length; f += 1) {
+            title = films[f].getElementsByTagName("h3")[0];
+            title.style.cursor = "pointer"; //Let's user know they can click on title
+            title.onclick = showHideDetails;
         }
-    }
-}
-function setup() {
-    var films, f, title;
-    films = document.getElementsByClassName("film");
-    for (f = 0; f < films.length; f += 1) {
-        title = films[f].getElementsByTagName("h3")[0];
-        title.style.cursor = "pointer"; //Let's user know they can click on title
-        title.onclick = showHideDetails;
-    }
-}
+    };
+
+    return pub;
+
+}());
+
 if (document.getElementById) {
-    window.onload = setup;
+    if (window.addEventListener) {
+        window.addEventListener('load', showHide.setup);
+    } else if (window.attachEvent) {
+        window.attachEvent('onload', showHide.setup);
+    } else {
+        alert("Could not attach 'showHide.setup' to the 'window.onload' event");
+    }
 }
