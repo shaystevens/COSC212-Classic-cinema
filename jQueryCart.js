22be64d34897ff4ca1eba98668eb6cd0fe5e393e
sstevens <stesh969@student.otago.ac.nz>
2021-08-12 14:05:46 +1200

    Lab 10 start

---
+++ jQueryCart.js
@@ -0,0 +1,84 @@
+var cart = (function(){
+    "use strict";
+    var pub = {};
+    var movie;
+    var movieArray = [];
+
+    function addToCart(){
+        movie = {
+            title: this.parentElement.parentElement.querySelector("h3").innerHTML ,
+            price: this.parentElement.querySelector("span").innerHTML
+        };
+
+        movieArray.push(movie);
+        if(Cookie.get("Movie") === null){
+            Cookie.set("Movie", JSON.stringify(movieArray), 1);
+        } else{
+            movieArray = Cookie.get("Movie");
+            movieArray = JSON.parse(movieArray);
+            movieArray.push(movie);
+            Cookie.set("Movie",  JSON.stringify(movieArray), 1);
+        }
+    }
+
+    pub.setup = function(){
+        var buyButton, films, f;
+        films = $("film");
+        for (f = 0; f < films.length; f += 1) {
+            $("#buttonID").click(eventHandler);
+            buyButton = films[f].getElementsByClassName("buy")[0];
+            buyButton.style.cursor = "pointer"; //Let's user know they can click on title
+            buyButton.onclick = addToCart;
+        }
+    };
+
+    return pub;
+}());
+
+if (document.getElementById) {
+    if (window.addEventListener) {
+        window.addEventListener('load', cart.setup);
+    } else if (window.attachEvent) {
+        window.attachEvent('onload', cart.setup);
+        /* jshint ignore:start */
+    } else {
+        alert("Could not attach 'cart.setup' to the 'window.onload' event");
+        /* jshint ignore:end */
+    }
+}
+
+var Cookie = (function () {
+
+    var pub = {};
+    pub.set = function (name, value, hours) {
+        var date, expires;
+
+        if (hours) {
+            date = new Date();
+            date.setHours(date.getHours() + hours);
+            expires = "; expires=" + date.toGMTString();
+        } else {
+            expires = "";
+        }
+        document.cookie = name + "=" + value + expires + "; path=/";
+    };
+
+    pub.get = function (name) {
+        var nameEq, cookies, cookie, i;
+        nameEq = name + "=";
+        cookies = document.cookie.split(";");
+        for (i = 0; i < cookies.length; i += 1) {
+            cookie = cookies[i].trim();
+            if (cookie.indexOf(nameEq) === 0) {
+                return cookie.substring(nameEq.length, cookie.length);
+            }
+        }
+        return null;
+    };
+
+    pub.clear = function (name) {
+        pub.set(name, "", -1);
+    };
+
+    return pub;
+}());
